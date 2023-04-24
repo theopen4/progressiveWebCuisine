159fed2765dbd8beaa50dc5c5288a247981e4b80
theopen4 <kielze86@gmail.com>
2023-04-24 15:16:05 +0100

    init mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
