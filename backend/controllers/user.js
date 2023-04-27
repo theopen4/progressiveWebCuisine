b4f8e31638bba01c13f488f7e5a20c98598db103
theopen4 <kielze86@gmail.com>
2023-04-27 22:44:03 +0100

    add routes login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -13,4 +13,24 @@ exports.signup = (req, res, next) => {
       })
       .catch(error => res.status(500).json({ error }));
   };
-exports.login = ()=>{};
+exports.login = (req, res, next)=>{
+
+   User.findOne({email: req.body.email})
+      .then(user => {
+          if(!user){
+              return res.status(401).json({message:'paire login ou mot de passe incorrecte'});  
+          }
+          bcrypt.compare(req.body.password, user.password) 
+              .then(valid =>{
+                if(!valid){
+                    return res.status(401).json({message:'paire login ou mot de passe incorrecte'})
+                }
+                res.status(200).json({
+                  userId: user._id,
+                  token: 'TOKEN'
+                })   
+              })
+              .catch(error => res.status(500).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error }));
+};
